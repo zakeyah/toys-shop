6717b8b744474dd144e25b34be1ceb54bf01f6f1
zakeyah <mom11omar@gmail.com>
2021-01-27 16:59:22 +0200

    js

--- add-content.js
+++ add-content.js
@@ -1,13 +1,14 @@
-var childage=prompt("How old is youer kid ?");
-var brand=prompt("what kinde of brand are you looking for ?");
+var childage = prompt("How old is youer kid ?");
+var brand = prompt("what kinde of brand are you looking for ?");
 
-var infermotion; 
+var infermotion;
 
-if(childage >=1 && childage <2){infermotion="you should go to Baby section"}
-else if(childage >=3 && childage <10){infermotion="you should go to children section"}
-else if(childage >=10){infermotion="you should go to teenagers section"};
+if (childage >= 1 && childage < 2) { infermotion = "you should go to Baby section" }
+else if (childage >= 3 && childage < 10) { infermotion = "you should go to children section" }
+else if (childage >= 10) { infermotion = "you should go to teenagers section" };
+ 
 
 
 document.write(infermotion);
 confirm("DO YOU WANT A HELP FROM ON OF OUR WORKER ?");
-alart("welcome to our shop");
+alert("welcome to our shop");
