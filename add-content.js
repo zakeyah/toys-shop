6eb49d7abfdff6d8105e6ff99050b0a94d6111e4
zakeyah <mom11omar@gmail.com>
2021-01-28 13:49:04 +0200

    loob

--- add-content.js
+++ add-content.js
@@ -1,14 +1,37 @@
 var childage = prompt("How old is youer kid ?");
-var brand = prompt("what kinde of brand are you looking for ?");
 
 var infermotion;
 
-if (childage >= 1 && childage < 2) { infermotion = "you should go to Baby section" }
-else if (childage >= 3 && childage < 10) { infermotion = "you should go to children section" }
-else if (childage >= 10) { infermotion = "you should go to teenagers section" };
- 
+if (childage >= 1 && childage < 2) {
+    infermotion = "you should go to Baby section" }
+else if (childage >= 3 && childage < 10) {
+    infermotion = "you should go to children section" }
+else if (childage >= 10) {
+    infermotion = "you should go to teenagers section" };
+
+    document.write(infermotion);
+
+var brand = prompt("what brand do yo want lego or barbie ?") ;
+
+while( brand !== "lego" && brand !== "barbie" ){
+brand = prompt("what brand do yo want lego or barbie ?") ;
+}
+
+var brandkind = "" ;
+if ( brand === "lego"){
+    brandkind = "<img src ='images/lego.png'/>" ;
+} else if ( brand === "barbie"){
+    brandkind = "<img src ='images/barbie.jpg'/>" ;  
+}
+
+var notoys = prompt("how many toy you want ?") ; 
+
+var result = "";
+for ( var i=0 ; i<notoys ; i++ ){
+result = result + brandkind ;
+}
 
 
-document.write(infermotion);
+document.write(result);
 confirm("DO YOU WANT A HELP FROM ON OF OUR WORKER ?");
 alert("welcome to our shop");
